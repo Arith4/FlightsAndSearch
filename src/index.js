1b4cb68ac41694db7dd76fbe9ebefbf4c4120805
Arith <arithdas100@gmail.com>
2023-03-08 23:07:19 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
 const ApiRoutes = require("./routes/index");
+const db = require("./models/index");
 
 const setUpAndStartServer = async () => {
   const app = express();
@@ -14,6 +15,9 @@ const setUpAndStartServer = async () => {
 
   app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
+    if(process.env.SYNC_DB){
+      db.sequelize.sync({alter:true});
+    }
   });
 };
 
